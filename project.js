0b87949a33da07f670151e18755aeedd66bbf8cb
Omolemo Modise <omolemo.modise19@gmail.com>
2024-12-06 00:30:22 +0200

    Add spin and transpose functions

--- project.js
+++ project.js
@@ -1,7 +1,22 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLUMNS = 3;
 
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8
+}
+
+const SYMBOLS_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2
+}
 
 
 
@@ -59,6 +74,53 @@ const getBet = (balance, lines) => {
   }
 }
 
+
+const spin = () => {
+  const symbols = [];
+  for(const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+    for(let i = 0; i < count; i++){
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [];
+
+  for(let i = 0; i < COLUMNS; i++){
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for(let j = 0; j < ROWS; j++){
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex,1);
+
+    }
+  }
+  return reels;
+}
+
+const transpose = (reels) =>{
+  const rows = []
+
+  for(let i = 0; i < ROWS; i++){
+    rows.push([]);
+    for(let j = 0; j < COLUMNS; j++){
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+}
+
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines)
+const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+
+
+
+
+
+
